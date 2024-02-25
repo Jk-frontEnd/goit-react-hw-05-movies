ef89039ecc76d231f8cdb35cd0bd72335f70817f
JulianskyMe <juliakamynskaya@gmail.com>
2024-02-25 19:52:50 +0200

    yeah

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Router basename="/goit-react-05-movies">
+      <BrowserRouter basename="/goit-react-05-movies">
         <App />
-      </Router>
+      </BrowserRouter>
   </React.StrictMode>
 );
