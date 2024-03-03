6293fcbc7603b1a41eadd8194f7fbc66788cceba
Jk-frontEnd <151435676+Jk-frontEnd@users.noreply.github.com>
2024-03-03 12:38:01 +0000

    Deploying to gh-pages from @ Jk-frontEnd/goit-react-hw-05-movies@b4bc942921506a67d28fb77cd56972bec5e4a1a8 🚀

--- static/js/906.a78a9f62.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[906],{906:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var r=a(87);const c="BackLink_backLink__JiXo7";var k=a(184);const s=e=>{let{children:t,to:a}=e;return(0,k.jsx)(r.rU,{to:a,className:c,children:t})}}}]);
-//# sourceMappingURL=906.a78a9f62.chunk.js.map
